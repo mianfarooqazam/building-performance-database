18307e62bd762241112682ef8a15069914c36978
Farooq <30216781+mianfarooqazam@users.noreply.github.com>
2024-10-09 15:31:31 +0500

    RoofLayerData updated

--- src/utils/RoofLayerData.js
+++ src/utils/RoofLayerData.js
@@ -1,17 +1,76 @@
-const OuterLayer = ["Cement plaster (sand aggregate)", "Gypsum plaster (sand aggregate)", "Gypsum plaster (Vermiculite aggregate)", "plywood", "sheathing (regular density)", " Acustic tile", "Hard board (siding)", " Hard board (High density)",
-  "Particle board (low density)", "Particle board (high density)", "Hard wood (oak,maple)", "Soft wood (fir, pine)", "clay tiles (3 cell deep)", "clay tiles (1 cell deep)", " Cement lime", " Cement mortar"
+const OuterLayer = [
+  { name: "Cement plaster (sand aggregate)", k_value: 0.72 },
+  { name: "Gypsum plaster (sand aggregate)", k_value: 0.22 },
+  { name: "Gypsum plaster (Vermiculite aggregate)", k_value: 0.25 },
+  { name: "Plywood", k_value: 0.12 },
+  { name: "Sheathing (regular density)", k_value: 0.055 },
+  { name: "Acoustic tile", k_value: 0.058 },
+  { name: "Hard board (siding)", k_value: 0.094 },
+  { name: "Hard board (High density)", k_value: 0.15 },
+  { name: "Particle board (low density)", k_value: 0.078 },
+  { name: "Particle board (high density)", k_value: 0.17 },
+  { name: "Hard wood (oak, maple)", k_value: 0.16 },
+  { name: "Soft wood (fir, pine)", k_value: 0.12 },
+  { name: "Clay tiles (3 cell deep)", k_value: 0.52 },
+  { name: "Clay tiles (1 cell deep)", k_value: 0.69 },
+  { name: "Cement lime", k_value: 1.4 },
+  { name: "Cement mortar", k_value: 0.65 },
+  { name: "Stucco", k_value: 0.72 },
 ];
 
+
+
+// refrence usman (obviouslu pick higher k-value than k-lower for same names)
 const CoreLayer = [
-  "Brick Common", "Brick, face", "Brick, fire clay", "Concrete Block 3 oval cores", "Concrete Block rectangular core (same with filled cores)", "Concrete Block rectangular core (2 cores 20 cm thick, 16 kg)", "Concrete Block 3 oval core cinder aggregate", "Concrete: Lightweight aggregates", "Concrete: Lightweight aggregates (expanded slags)"
+  { name: " Brick Common", k_value: 0.72 },
+  { name: " Brick, face", k_value: 1.3 },
+  { name: " Brick, fire clay", k_value: 1.34 },
+  { name: " Concrete Block 3 oval cores", k_value: 1 },
+  { name: " Concrete Block rectangular core (same with filled cores)", k_value: 0.6 },
+  { name: " Concrete Block rectangular core (2 cores 20 cm thick, 16 kg)", k_value: 1.1 },
+  { name: " Concrete Block 3 oval core cinder aggregate", k_value: 0.67 },
+  { name: " Concrete: Lightweight aggregates", k_value: 1.1 },
+  { name: " Concrete: Lightweight aggregates (expanded slags)", k_value: 0.79 }
 ];
 
-const InsulationLayer = ["Air Space", "Poly Styrene (R-12)", "Poly Styrene (molded beads)", "Polyurethane Foam", "Mineral Fiber Board (roofing material)", "Wood (shredded cemented)", "Cork", "Loose Fill Material (cork granulated)", "Blanket and Batt (paper faced)", "Glass Fiber (coated)", "Glass Fiber (organic bonded)", "Cellular Glass", "Polyethylene/Jumbolon"
+
+
+
+const InsulationLayer = [
+  { name: "Air Space", k_value: 0.01516 },
+  { name: "Poly Styrene (R-12)", k_value: 0.027 },
+  { name: "Poly Styrene (molded beads)", k_value: 0.04 },
+  { name: "Polyurethane Foam", k_value: 0.025 },
+  { name: "Mineral Fiber Board (roofing material)", k_value: 0.049 },
+  { name: "Wood (shredded cemented)", k_value: 0.087 },
+  { name: "Cork", k_value: 0.039 },
+  { name: "Loose Fill Material (cork granulated)", k_value: 0.045 },
+  { name: "Blanket and Batt (paper faced)", k_value: 0.46 },
+  { name: "Glass Fiber (coated)", k_value: 0.038 },
+  { name: "Glass Fiber (organic bonded)", k_value: 0.036 },
+  { name: "Cellular Glass", k_value: 0.058 },
+  { name: "Polyethylene/Jumbolon", k_value: 0.33 }
 ];
 
 
-const InnerLayer = ["Cement plaster (sand aggregate)", "Gypsum plaster (sand aggregate)", "Gypsum plaster (Vermiculite aggregate)", "plywood", "sheathing (regular density)", " Acustic tile", "Hard board (siding)", " Hard board (High density)",
-  "Particle board (low density)", "Particle board (high density)", "Hard wood (oak,maple)", "Soft wood (fir, pine)", "clay tiles (3 cell deep)", "clay tiles (1 cell deep)", " Cement lime", " Cement mortar"
+const InnerLayer = [
+  { name: "Cement plaster (sand aggregate)", k_value: 0.72 },
+  { name: "Gypsum plaster (sand aggregate)", k_value: 0.22 },
+  { name: "Gypsum plaster (Vermiculite aggregate)", k_value: 0.25 },
+  { name: "Plywood", k_value: 0.12 },
+  { name: "Sheathing (regular density)", k_value: 0.055 },
+  { name: "Acoustic tile", k_value: 0.058 },
+  { name: "Hard board (siding)", k_value: 0.094 },
+  { name: "Hard board (High density)", k_value: 0.15 },
+  { name: "Particle board (low density)", k_value: 0.078 },
+  { name: "Particle board (high density)", k_value: 0.17 },
+  { name: "Hard wood (oak, maple)", k_value: 0.16 },
+  { name: "Soft wood (fir, pine)", k_value: 0.12 },
+  { name: "Clay tiles (3 cell deep)", k_value: 0.52 },
+  { name: "Clay tiles (1 cell deep)", k_value: 0.69 },
+  { name: "Cement lime", k_value: 1.4 },
+  { name: "Cement mortar", k_value: 0.65 },
+  { name: "Stucco", k_value: 0.72 },
 ];
 
 
