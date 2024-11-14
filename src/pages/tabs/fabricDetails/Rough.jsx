bee31978a006af5da011be22d007484ed0e54ac7
Farooq <30216781+mianfarooqazam@users.noreply.github.com>
2024-11-14 10:55:33 +0500

    Roof Layer data updated, comments removed

---
+++ src/pages/tabs/fabricDetails/Rough.jsx
@@ -0,0 +1,66 @@
+// RoofCalculation.js
+
+export function calculateRValue(thicknessInches, kValue) {
+    // Convert thickness from inches to meters
+    const thicknessMeters = thicknessInches * 0.0254;
+  
+    // Calculate R-value
+    const rValue = thicknessMeters / kValue;
+  
+    return rValue;
+  }
+  
+  export function calculateRTotal(rValues, hi = 2.5, ho = 11.54) {
+    // Ensure hi and ho are valid numbers greater than zero
+    const hiValue = parseFloat(hi);
+    const hoValue = parseFloat(ho);
+  
+    if (hiValue <= 0 || isNaN(hiValue)) {
+      throw new Error("Invalid value for hi. It must be a number greater than zero.");
+    }
+  
+    if (hoValue <= 0 || isNaN(hoValue)) {
+      throw new Error("Invalid value for ho. It must be a number greater than zero.");
+    }
+  
+    // Calculate surface resistances
+    const rHi = 1 / hiValue;
+    const rHo = 1 / hoValue;
+  
+    // Sum the R-values
+    const sumRValues = rValues.reduce((sum, rValue) => sum + rValue, 0);
+  
+    // Add inverses of hi and ho
+    const rTotal = sumRValues + rHi + rHo;
+  
+    return rTotal;
+  }
+  
+  export function calculateUValue(rTotal) {
+    // Calculate U-value
+    const uValue = 1 / rTotal;
+    return uValue;
+  }
+  
+  export function calculateKappaValue(layers) {
+    if (layers.length > 0) {
+      const sum = layers.reduce((accumulator, layer) => {
+        const { thicknessInches, shValue, dValue } = layer;
+        if (
+          thicknessInches != null &&
+          !isNaN(thicknessInches) &&
+          shValue != null &&
+          !isNaN(shValue) &&
+          dValue != null &&
+          !isNaN(dValue)
+        ) {
+          return accumulator + thicknessInches * shValue * dValue;
+        }
+        return accumulator;
+      }, 0);
+      return (sum / 1000).toFixed(3);
+    } else {
+      return null;
+    }
+  }
+  
