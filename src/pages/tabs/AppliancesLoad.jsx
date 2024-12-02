109ccd289b220aa3da4fd18036b17fd3695e5c36
Farooq <30216781+mianfarooqazam@users.noreply.github.com>
2024-12-02 14:32:12 +0500

    Appliances data added, page modified

--- src/pages/tabs/AppliancesLoad.jsx
+++ src/pages/tabs/AppliancesLoad.jsx
@@ -1,10 +1,121 @@
-import { Box } from '@mui/material';
+import { useState } from 'react';
+import {
+  Box,
+  TextField,
+  Select,
+  MenuItem,
+  InputLabel,
+  FormControl
+} from '@mui/material';
+import {
+  FanManufacturer,
+  OvenManufacturer,
+  WashingMachineManufacturer,
+  RefrigeratorManufacturer,
+  VacuumManufacturer,
+  HeaterManufacturer,
+  IronManufacturer,
+  TVManufacturer,
+  WaterPumpManufacturer
+} from '../../utils/appliances/AppliancesData.js'; 
+
+const applianceOptions = [
+  'Fan',
+  'Oven',
+  'Washing Machine',
+  'Refrigerator',
+  'Vacuum',
+  'Heater',
+  'Iron',
+  'TV',
+  'Water Pump'
+];
+
+const manufacturerMap = {
+  Fan: FanManufacturer,
+  Oven: OvenManufacturer,
+  'Washing Machine': WashingMachineManufacturer,
+  Refrigerator: RefrigeratorManufacturer,
+  Vacuum: VacuumManufacturer,
+  Heater: HeaterManufacturer,
+  Iron: IronManufacturer,
+  TV: TVManufacturer,
+  'Water Pump': WaterPumpManufacturer
+};
 
 function AppliancesLoad() {
+  const [appliance, setAppliance] = useState('');
+  const [manufacturer, setManufacturer] = useState('');
+  const [quantity, setQuantity] = useState('');
+  const [dailyHourUsage, setDailyHourUsage] = useState('');
+
   return (
-    <Box p={3}>
-      <p>appliances details and fields...</p>
-      {/* Add inputs related to fabric details */}
+    <Box p={3} maxWidth={800} margin="0 auto">
+      <Box display="flex" flexDirection="column" gap={2}>
+        {/* Appliance Input */}
+        <FormControl fullWidth required>
+          <InputLabel id="appliance-label">Appliance</InputLabel>
+          <Select
+            labelId="appliance-label"
+            value={appliance}
+            label="Appliance"
+            onChange={(e) => {
+              setAppliance(e.target.value);
+              setManufacturer('');
+            }}
+          >
+            {applianceOptions.map((item, index) => (
+              <MenuItem key={index} value={item}>
+                {item}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+
+        {/* Manufacturer Input */}
+        <FormControl fullWidth required disabled={!appliance}>
+          <InputLabel id="manufacturer-label">Manufacturer</InputLabel>
+          <Select
+            labelId="manufacturer-label"
+            value={manufacturer}
+            label="Manufacturer"
+            onChange={(e) => setManufacturer(e.target.value)}
+          >
+            {appliance &&
+              manufacturerMap[appliance].map((manu, index) => (
+                <MenuItem key={index} value={manu}>
+                  {manu}
+                </MenuItem>
+              ))}
+          </Select>
+        </FormControl>
+
+        <Box display="flex" flexDirection={{ xs: 'column', sm: 'row' }} gap={2}>
+          {/* Quantity Input */}
+          <TextField
+            label="Quantity"
+            variant="outlined"
+            fullWidth
+            required
+            type="number"
+            slotProps={{ htmlInput: { min: 1 } }}
+            value={quantity}
+            onChange={(e) => setQuantity(e.target.value)}
+          />
+
+          {/* Daily Hour Usage Input */}
+          <TextField
+            label="Daily Hour Usage"
+            variant="outlined"
+            fullWidth
+            required
+            type="number"
+            slotProps={{ htmlInput: { min: 0, max: 24 } }}
+            value={dailyHourUsage}
+            onChange={(e) => setDailyHourUsage(e.target.value)}
+          />
+        </Box>
+      </Box>
     </Box>
   );
 }
---
+++ src/utils/appliances/AppliancesData.js
@@ -0,0 +1,120 @@
+const FanManufacturer = [
+    "Pak Fans",
+    "GFC",
+    "Royal",
+    "SK Fans",
+    "Khurshid Fans",
+    "Royal"
+];
+
+const OvenManufacturer = [
+    "Dawlance",
+    "Ecostar",
+    "Anex",
+    "Sharp",
+    "LG",
+    "Panasonic",
+    "Samsung",
+    "Haier",
+    "Kenwood",
+    "GREE",
+    "PEL",
+    "Waves"
+];
+
+const WashingMachineManufacturer = [
+    "Super Asia",
+    "Kenwood",
+    "Dawlance",
+    "Haier",
+    "PEL",
+    "LG",
+    "Kenwood"
+];
+
+const RefrigeratorManufacturer = [
+    "Haier",
+    "Dawlance",
+    "Orient",
+    "Haier",
+    "LG",
+    "Panasonic",
+    "Orient",
+    "Kenwood",
+    "LG",
+    "Orient",
+    "Samsung",
+    "PEL"
+];
+
+
+
+const VacuumManufacturer = [
+    "Philips",
+    "National",
+    "Panasonic",
+    "PEL",
+    "Dawlance",
+    "National",
+    "Super Asia",
+    "National"
+];
+
+const HeaterManufacturer = [
+    "SOGO",
+    "National",
+    "Anex",
+    "Dawlance",
+    "E-Lite",
+    "LG",
+    "Dawlance"
+];
+
+const IronManufacturer = [
+    "Panasonic",
+    "Kenwood",
+    "Philips",
+    "Westpoint",
+    "Super Asia",
+    "LG",
+    "Dawlance",
+    "Sony",
+    "Orient"
+];
+
+const TVManufacturer = [
+    "TLC",
+    "Haier",
+    "Castle",
+    "Hisense",
+    "Shalimar",
+    "Infinity",
+    "Super Asia",
+    "Orient",
+    "Samsung"
+];
+
+const WaterPumpManufacturer = [
+    "Golden",
+    "GFC",
+    "Castle",
+    "Hi-Flow",
+    "Shalimar",
+    "Infinity",
+    "Super Asia",
+    "Orient",
+    "Shalimar"
+];
+
+
+export {
+    FanManufacturer,
+    OvenManufacturer,
+    WashingMachineManufacturer,
+    RefrigeratorManufacturer,
+    VacuumManufacturer,
+    HeaterManufacturer,
+    IronManufacturer,
+    TVManufacturer,
+    WaterPumpManufacturer
+};
